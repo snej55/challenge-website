42866d0c5049db7238252a3e7e4a7ae42a48b166
snej55 <jenskromdijk@gmail.com>
2025-09-08 10:47:19 +0100

    Add react-router-dom to package.json

--- site/src/App.js
+++ site/src/App.js
@@ -1,27 +1,30 @@
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <img src="assets/images/CASTlogo.png" alt="cast logo"/>
-        <h3 class="header-title">Cambridge Academy for Science and Technology WIKI</h3>
-      </header>
-      <main>
-          <h2>Content to be added:</h2><br />
-          <ul>
-              <li>School homework/revision websites with info on each of them</li><br />
-              <li>Location of useful things, such as water fountains</li><br />
-              <li>Basic overview of school rules</li><br />
-              <li>uniform and equipment policy</li><br />
-              <li>school day timings</li><br />
-              <li>other useful info</li><br />
-          </ul>
-      </main>
-      <footer>
-          <p>&copy; 2025 Challenge Website!</p>
-      </footer>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header>
+          <img src="assets/images/CASTlogo.png" alt="cast logo"/>
+          <h3 class="header-title">Cambridge Academy for Science and Technology WIKI</h3>
+        </header>
+        <main>
+            <h2>Content to be added:</h2><br />
+            <ul>
+                <li>School homework/revision websites with info on each of them</li><br />
+                <li>Location of useful things, such as water fountains</li><br />
+                <li>Basic overview of school rules</li><br />
+                <li>uniform and equipment policy</li><br />
+                <li>school day timings</li><br />
+                <li>other useful info</li><br />
+            </ul>
+        </main>
+        <footer>
+            <p>&copy; 2025 Challenge Website!</p>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
