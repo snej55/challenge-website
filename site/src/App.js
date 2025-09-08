701d5a5ebb02b6efe09a42392ef4b09f5f85b1e9
snej55 <jenskromdijk@gmail.com>
2025-09-08 10:37:02 +0100

    Add styles

--- site/src/App.js
+++ site/src/App.js
@@ -4,20 +4,23 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h3 class="header-title">Cambridge Academy for Science and Technology WIKI</h3>
       </header>
+      <main>
+          <h2>Content to be added:</h2><br />
+          <ul>
+              <li>School homework/revision websites with info on each of them</li><br />
+              <li>Location of useful things, such as water fountains</li><br />
+              <li>Basic overview of school rules</li><br />
+              <li>uniform and equipment policy</li><br />
+              <li>school day timings</li><br />
+              <li>other useful info</li><br />
+          </ul>
+      </main>
+      <footer>
+          <p>&copy; 2025 Challenge Website!</p>
+      </footer>
     </div>
   );
 }
