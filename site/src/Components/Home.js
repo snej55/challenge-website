d928f84964c7116fab5668888397999478d3e873
Tom Dunne <thomasmathewdunne@gmail.com>
2025-09-09 09:39:10 +0100

    Text spacing

--- site/src/Components/Home.js
+++ site/src/Components/Home.js
@@ -3,7 +3,7 @@ import './Home.css'
 export function Home()
 {
     return (
-        <div>
+        <div className="home-container">
             <h2>Welcome to the Wiki</h2><br />
             This wiki is a non-profit community project, launched by 5 year 11s who wanted to make things easier for others.<br/>
             Like you, we once started at this school, and felt confused by the bustling hive of activity and learning.<br/>
