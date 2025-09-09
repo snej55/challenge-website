0833b4239494441737e96d33f25b078fd694a2f0
Cheerio2 <sebastian.j.thornton@outlook.com>
2025-09-09 09:59:29 +0100

    Pushing!

--- site/src/Components/Games.js
+++ site/src/Components/Games.js
@@ -4,6 +4,7 @@ export function Games() {
     return (
         <div className="Games">
             Games page
+            <iframe src="https://editor.p5js.org/snej55/sketches/pq3lk0NZH" width="1000" height="750" allowfullscreen></iframe>
         </div>
     );
 }
