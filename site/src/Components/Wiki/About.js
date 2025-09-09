36ea339db90a9fd3c73dfcad6d10ddd4fe4b191c
zibuyin <natdrone101@gmail.com>
2025-09-09 17:19:39 +0100

    Fixed footer, added info cards

--- site/src/Components/Wiki/About.js
+++ site/src/Components/Wiki/About.js
@@ -1,6 +1,6 @@
 import './About.css';
 import './wiki-pages.css'
-
+import './info-card.css'
 export function About() {
     return (
 
@@ -26,6 +26,19 @@ export function About() {
                 them how to do basic things, like where is the water fountain, or how to submit homework.</p>
             <p>Because of this, we decided that a WIKI about the school should be made. The objective of the guide is to
                 make students' everyday life around the school smoother and easier. </p>
+            <div className="spacer-20"></div>
+            <div id="creative" class="info-card card-gradient-orange-red">
+                <h3>Why was this WIKI Made?</h3>
+
+                <p>This page was made by five Year 11 CAST students. We realised that many students
+                    new to our school were not
+                    able to find their way around the school easily, which leaded to the teachers needing to spend days teaching
+                    them how to do basic things, like where is the water fountain, or how to submit homework.</p>
+                <p>Because of this, we decided that a WIKI about the school should be made. The objective of the guide is to
+                    make students' everyday life around the school smoother and easier. </p>
+
+            </div>
+
         </div>
     );
 }
