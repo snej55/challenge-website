c4e004ff3bca6e306c4a1f2bb5daf16e65998b5a
Tom Dunne <thomasmathewdunne@gmail.com>
2025-09-09 10:11:11 +0100

    update

--- site/src/Components/Wiki/SchoolPolicies.js
+++ site/src/Components/Wiki/SchoolPolicies.js
@@ -4,6 +4,16 @@ export function SchoolPolicies() {
     return (
         <div>
             📄 School Policies
+            <h1>Uniform</h1>
+            
+            
+            <ul>
+                <h3>Here at CAST, the uniform is comfortable and stylish, but quite restricted nonetheless.</h3>
+                <li>We are expected to wear grey trousers, or grey shorts during the warm times of year.</li>
+                <li>White shirts, short or long sleeve, are allowed, but no polo shirts.</li>
+                <li>The school jumper (the only one allowed) </li>
+
+            </ul>
         </div>
     );
 }
