ffe06536ec206b818a76fb20e277103c13b1145b
Tom Dunne <thomasmathewdunne@gmail.com>
2025-09-09 09:10:40 +0100

    More home page text

--- site/src/Components/Home.js
+++ site/src/Components/Home.js
@@ -6,9 +6,10 @@ export function Home()
         <div>
             <h2>Welcome to the Wiki</h2><br />
             This wiki is a non-profit community project, launched by 5 year 11s who wanted to make things easier for others.<br/>
-            Like you, we once started at this school, and felt confused by the bustling hive of activity and learning<br/>
+            Like you, we once started at this school, and felt confused by the bustling hive of activity and learning.<br/>
             But now, you don't have to! Because this wiki is here to help.<br/>
-            
+            Packed with games, flashcards, and an exxhaustive amount of info on every school related subject you could name, we're here to assist in any way we can.<br/>
+            We've poured our heart and soul into this webpage, and would love to see it help. Have fun!<br/>
             
         </div>
     );
