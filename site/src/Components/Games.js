1beac662c1a03d74d57b8750efa67677aad37f9e
Cheerio2 <sebastian.j.thornton@outlook.com>
2025-09-09 10:17:56 +0100

    Will this work?

--- site/src/Components/Games.js
+++ site/src/Components/Games.js
@@ -4,7 +4,7 @@ export function Games() {
     return (
         <div className="Games-Text">
             Games page
-            <iframe src="https://editor.p5js.org/sebastianthornton/full/jFhNQftjk"></iframe>
+            <iframe src="https://editor.p5js.org/sebastianthornton/full/jFhNQftjk" width="600" height="480" allowfullscreen></iframe>
         </div>
     );
 }
