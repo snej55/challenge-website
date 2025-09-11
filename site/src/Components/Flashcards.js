aae47350503bb784e5d22a60c2f4c333086668b9
snej55 <jenskromdijk@gmail.com>
2025-09-11 19:41:38 +0100

    Start work on flashcards

--- site/src/Components/Flashcards.js
+++ site/src/Components/Flashcards.js
@@ -1,9 +1,31 @@
 import "./Flashcards.css";
 
+import { useState } from 'react';
+
 export function Flashcards() {
+    const [cards, setCards] = useState([]);
+
+    // question, answer
+    function addCard(q, a)
+    {
+        setCards(
+            [
+                {question: q, answer: a},
+                ...cards
+            ]
+        )
+    }
+
     return (
-        <div className="Flashcards">
-            Flashcards page
+        <div className="flashcards-container">
+            <h1>Flashcards</h1>
+
+            <footer>
+                <button>Add flashcard</button>
+                <button>Save flashcards</button>
+                <button>Open flashcards</button>
+                <button>Clear flashcards</button>
+            </footer>
         </div>
     );
 }
