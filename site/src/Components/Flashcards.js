b4ed61760f78e8bb785bd9832831fc949da9a75a
snej55 <jenskromdijk@gmail.com>
2025-09-12 12:58:01 +0100

    Tidy up

--- site/src/Components/Flashcards.js
+++ site/src/Components/Flashcards.js
@@ -69,30 +69,6 @@ export function Flashcards() {
 
     return (
         <div className="flashcards-container">
-            {/* <div className="left-panel">
-                <div className="left-panel-header">
-                    <h1>Flashcards</h1>
-
-                </div>
-                <div></div>
-            </div>
-
-            <div className="right-panel">
-
-            </div> */}
-            {/* <div className="flashcards-header">
-                <h1 className="flashcards-title">Flashcards</h1>
-                <div className="flashcards-header-button-div">
-                    <button >Save flashcards</button>
-                    <button>Open flashcards</button>
-                    <button>Clear flashcards</button>
-                </div>
-            </div>
-            <div className="flashcards-right-panel">
-                <textarea className="question-input"></textarea>
-                <textarea className="answer-input"></textarea>
-                <button>Add flashcard</button>
-            </div> */}
             <div className="flashcard-left-panel">
                 <h1 className="flashcards-title">Flashcards</h1>
                 <div className="flashcards-left-panel-button-div">
