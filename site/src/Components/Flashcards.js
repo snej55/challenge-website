82f19adf225e8c75ecfd1164155c247bac8cc325
zibuyin <natdrone101@gmail.com>
2025-09-12 12:50:36 +0100

    Added stuff

--- site/src/Components/Flashcards.js
+++ site/src/Components/Flashcards.js
@@ -6,11 +6,10 @@ export function Flashcards() {
     const [cards, setCards] = useState([]);
 
     // question, answer
-    function addCard(q, a)
-    {
+    function addCard(q, a) {
         setCards(
             [
-                {question: q, answer: a},
+                { question: q, answer: a },
                 ...cards
             ]
         )
@@ -18,23 +17,41 @@ export function Flashcards() {
 
     return (
         <div className="flashcards-container">
-            <div className="left-panel">
+            {/* <div className="left-panel">
                 <div className="left-panel-header">
                     <h1>Flashcards</h1>
-                    <div className="left-panel-header-buttons">
-                        <button >Save flashcards</button>
-                        <button>Open flashcards</button>
-                        <button>Clear flashcards</button>
-                    </div>
+
                 </div>
                 <div></div>
             </div>
 
             <div className="right-panel">
+
+            </div> */}
+            {/* <div className="flashcards-header">
+                <h1 className="flashcards-title">Flashcards</h1>
+                <div className="flashcards-header-button-div">
+                    <button >Save flashcards</button>
+                    <button>Open flashcards</button>
+                    <button>Clear flashcards</button>
+                </div>
+            </div>
+            <div className="flashcards-right-panel">
                 <textarea className="question-input"></textarea>
                 <textarea className="answer-input"></textarea>
                 <button>Add flashcard</button>
+            </div> */}
+            <div className="flashcard-left-panel">
+                <h1 className="flashcards-title">Flashcards</h1>
+                <div className="flashcards-left-panel-button-div">
+                    <button >Save flashcards</button>
+                    <button>Open flashcards</button>
+                    <button>Clear flashcards</button>
+                </div>
+
             </div>
+
+
         </div>
     );
 }
