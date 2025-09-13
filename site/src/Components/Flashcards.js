e16b5772dd346546f62744213a84ee845b2e2196
snej55 <jenskromdijk@gmail.com>
2025-09-13 18:05:32 +0100

    Improve flashcards styling

--- site/src/Components/Flashcards.js
+++ site/src/Components/Flashcards.js
@@ -12,10 +12,10 @@ function Flashcard({question, answer, cid})
                 <div class="flip-card">
                     <div class="flip-card-inner">
                         <div class="flip-card-front">
-                            <p>{question}</p>
+                            <p class="flashcard-question">{question}</p>
                         </div>
                         <div class="flip-card-back">
-                            <p>{answer}</p>
+                            <p class="flashcard-answer">{answer}</p>
                         </div>
                     </div>
                 </div>
@@ -49,6 +49,8 @@ export function Flashcards() {
             // clear text area
             document.getElementsByClassName("flashcards-textarea")[0].value = '';
             document.getElementsByClassName("flashcards-textarea")[1].value = '';
+            setAnswer('');
+            setQuestion('');
         }
     }
 
@@ -124,8 +126,8 @@ export function Flashcards() {
                     <div className="flashcards-left-panel-input-div">
                         <h3 className="flashcards-left-panel-h3">Questions Maker</h3>
                         <div className="flashcards-input-div">
-                            <textarea placeholder="Input Question..." className="flashcards-textarea question-input" onChange={e => setQuestion(e.target.value)}></textarea>
-                            <textarea placeholder="Input Answer..." className="flashcards-textarea answer-input" onChange={e => setAnswer(e.target.value)}></textarea>
+                            <textarea placeholder="Input Question..." maxLength="1000" className="flashcards-textarea question-input" onChange={e => setQuestion(e.target.value)}></textarea>
+                            <textarea placeholder="Input Answer..." maxLength="1000" className="flashcards-textarea answer-input" onChange={e => setAnswer(e.target.value)}></textarea>
                             <button className="flashcards-buttons green-button" onClick={addCard}>Add Card</button>
                         </div>
                     </div>
