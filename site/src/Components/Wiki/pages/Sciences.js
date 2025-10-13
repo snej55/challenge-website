7feb0285dd00f541022af9620ea6802c6af375db
snej55 <jenskromdijk@gmail.com>
2025-10-13 10:15:26 +0100

    Fix Jan's spelling mistakes

--- site/src/Components/Wiki/pages/Sciences.js
+++ site/src/Components/Wiki/pages/Sciences.js
@@ -25,7 +25,7 @@ export default function Sciences() {
 
             <h2 className="paragraph-title">🌿 Biology</h2>
             <p>
-                There are a lot of opurtunities to learn biology in extracuricular actitivities and clubs at CAST.
+                There are a lot of opportunities to learn biology in extracurricular actitivities and clubs at CAST.
             </p>
             <br /><br />
 
