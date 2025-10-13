6b4f3ee2430eb37ad2ebe79d9ec390befa396c48
Tom Dunne <thomasmathewdunne@gmail.com>
2025-10-13 09:22:26 +0100

    Subjects 1.1

--- site/src/Components/Wiki/pages/SubjectsCourses.js
+++ site/src/Components/Wiki/pages/SubjectsCourses.js
@@ -28,7 +28,7 @@ export default function SubjectsCourses() {
             While you get to choose between many subjects, there are a few you just <i>have</i> to take.
             <h3>English</h3>
             A must-have at any school, even a UTC, English is a valuable lesson to learn, from  bolstering CVs to helping you better appreciate what you read.
-            In CAST, just like anywhere else, you'll learn two different types of english: English Language, and English Literature
+            In CAST, just like anywhere else, you'll learn two different types of english: English Language, and English Literature. 
             To learn more, head over to the English page elsewhere in the wiki!
             <h3>Maths</h3>
             Maths is a no-brainer for anyone looking into STEM - serving as the foundation for every bit of research out there.
@@ -36,7 +36,12 @@ export default function SubjectsCourses() {
             Here, the teachers go above and beyond in every way to help you learn.
             Some of the top Y11s even get to study Further Maths, pushing beyond conventional boundaries to reach into the world of A-levels.
             To find out more, just dash to the Maths page elsewhere in the wiki!
-
+            <h3>Science!</h3>
+            For any STEM students, science teaches you what you really want and need to know - it really makes up everything.
+            All students at CAST are expected to take Triple Science, be that Foundation or Higher.
+            It's a great way to learn more and prepare for the future!
+            To look further, just dive in at the Sciences page in this very wiki!
+                        
         </div>
 
     );
