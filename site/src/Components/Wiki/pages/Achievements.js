282606f9534da62aa66e9cab8ab3543f5bc31b1d
Tom Dunne <thomasmathewdunne@gmail.com>
2025-10-20 09:45:02 +0100

    Awards

--- site/src/Components/Wiki/pages/Achievements.js
+++ site/src/Components/Wiki/pages/Achievements.js
@@ -21,15 +21,32 @@ export default function Achievements() {
             <div className="spacer-20"></div>
             <h1 id="title">🏆 Achievements</h1>
 
-            <h2 id="subtitle">This is the subtitle, change me</h2>
+            <h2 id="subtitle">From weekly stars to yearly sparx, the medals just keep on coming.</h2>
 
+            <h2 className="paragraph-title">Positive points</h2>
 
-            
-      
+            <p>Positive points are symbols of dedication and resilience here at CAST, with teachers logging them on Arbor for positive actions, whether that be for one student, a small group, or even a whole class!</p>
+            <p>Earning positive points is no easy task, but hopefully most of you will begin to accumulate them throughout the year.</p>
+            <p>At the end of each term, those with the most are awarded prizes at a special assembly. What more could you want? (For those disappointed, the prize also comes with a significant amount of chocolate, which may be more to your taste)</p>
+            <p>Just keep on learning, and you'll keep on earning!</p>
 
-            <h2 className="paragraph-title">This is paragraph title</h2>
+            <br/>
+            <h2 className="paragraph-title">Star of the Week</h2>
 
-            <p>This is the paragraph</p>
+            <p>Some of you may be thinking you don't want to wait all term for recognition. Fear not, for this is where the Star of the Week awards come in!</p>
+            <p>The Star of the Week is one of the most prestigious awards to be given here at CAST, and earns the lucky bearer more than a few envious stares, alongside a pack of Haribos from Andrew, who hands out the awards.</p>
+            <p>One student from each of the four forms in each year receives the award once a week, having been selected by their form tutors.</p>
+            <p>It is announced at the end of assembly, and is highly regarded as the culmination of the struggles of many.</p>
+            <br/>
+            <h2 className="paragraph-title">Core values</h2>
+            <p>At CAST, there are three core values held high above others: Respect, Resilience, and Responsibility.</p>
+            <p>At the end of term assembly, awards are given to the students that most embodied each of the values.</p>
+            <p>Multiple students are selected for each, and are given certificates to show to later employers as proof of their decency and dedication.</p>
+            <br/>
+            <h2 className="paragraph-title">Miscellaneous awards</h2>
+            <p>Alongside the major awards mentioned, there are others just as prestigious but too many to name.</p>
+            <p>Sparx top achievers, GCSE Pod best listeners, you name it, and you'll be awarded it.</p>
+            <p>So get learning, and win some awards to remember!</p>
         </div>
 
     );
